8e8520989195ca63dd065648acea55c47e2cc05e
nayem-48ai <150437672+nayem-48ai@users.noreply.github.com>
2025-02-19 21:27:55 +0600

    Add files via upload

--- song_list.js
+++ song_list.js
@@ -2,238 +2,238 @@
 let All_song = [
    {
      name: "1. আমি তোমার মনের ভিতর..",
-     path: "mp3/1.mp3",
+     path: "mp3/A/1.mp3",
      img: "img/1.jpg",
      singer: "বাংলা গান"
    },
    {
      name: "2. Dard|Hindi Full Lyrics",
-     path: "mp3/2.mp3",
+     path: "mp3/A/2.mp3",
      img: "img/2.jpg",
      singer: "Hindi Songs"
    },
    {
      name: "3. Favorite|Full",
-     path: "mp3/3.mp3",
+     path: "mp3/A/3.mp3",
      img: "img/3.jpg",
      singer: "Exclusive"
    },
    {
      name: "4. Older",
-     path: "mp3/4.mp3",
+     path: "mp3/A/4.mp3",
      img: "img/4.jpg",
      singer: "Exclusive"
    },
    {
      name: "5. Kyh tah hain pal pal",
-     path: "mp3/5.mp3",
+     path: "mp3/A/5.mp3",
      img: "img/5.jpg",
      singer: "Hindi Song"
    },
    {
     name: "Lambiya Si Jusaiyan",
-    path: "mp3/6.mp3",
+    path: "mp3/A/6.mp3",
     img: "img/1.jpg",
     singer: "Hindi Songs"
    },
    {
      name: "6. Photo| Luka chupi",
-     path: "mp3/7.mp3",
+     path: "mp3/A/7.mp3",
      img: "img/7.jpg",
      singer: "Hindi Song"
    },
    {
      name: "8. Maan mera",
-     path: "mp3/8.mp3",
+     path: "mp3/A/8.mp3",
      img: "img/8.jpg",
      singer: "Hindi Song"
    },
    {
      name: "9. Aja ve mahiya",
-     path: "mp3/9.mp3",
+     path: "mp3/A/9.mp3",
      img: "img/9.jpg",
      singer: "Hindi Song"
    },
    {
      name: "10. Pal Pal Dil K Pas",
-     path: "mp3/10.mp3",
+     path: "mp3/A/10.mp3",
      img: "img/10.jpg",
      singer: "Hindi Song"
    },
    {
      name: "11. Shub|You and Me",
-     path: "mp3/11.mp3",
+     path: "mp3/B/11.mp3",
      img: "img/11.jpg",
      singer: "Hindi Song"
    },
    {
      name: "12. Dhinak Dhin Tana",
-     path: "mp3/12.mp3",
+     path: "mp3/B/12.mp3",
      img: "img/12.jpg",
      singer: "Hindi Song"
    },
    {
      name: "13. Tu mile , dil khile",
-     path: "mp3/13.mp3",
+     path: "mp3/B/13.mp3",
      img: "img/13.jpg",
      singer: "Hindi Song"
    },
    {
      name: "14. Zara Zara , mehek ta hain",
-     path: "mp3/14.mp3",
+     path: "mp3/B/14.mp3",
      img: "img/14.jpg",
      singer: "Hindi Song"
    },
    {
-     name: "15. Unknown",
-     path: "mp3/15.mp3",
+     name: "15. Bagal Wali|DJ",
+     path: "mp3/B/15.mp3",
      img: "img/15.jpg",
-     singer: "Unknown"
+     singer: "DJ"
     },
    {
-     name: "16. Unknown",
-     path: "mp3/16.mp3",
+     name: "16. Tohar Cahar E jawani|DJ",
+     path: "mp3/B/16.mp3",
      img: "img/16.jpg",
-     singer: "Unknown"
+     singer: "DJ|Remix"
     },
     {
-     name: "17. Unknown",
-     path: "mp3/17.mp3",
+     name: "17. Jhuk jhuk kare|DJ",
+     path: "mp3/B/17.mp3",
      img: "img/17.jpg",
-     singer: "Unknown"
+     singer: "DJ|Remix"
     },
     {
      name: "18. Unknown",
-     path: "mp3/18.mp3",
+     path: "mp3/B/18.mp3",
      img: "img/18.jpg",
      singer: "Unknown"
     },
     {
      name: "19. Unknown",
-     path: "mp3/19.mp3",
+     path: "mp3/B/19.mp3",
      img: "img/19.jpg",
-     singer: "Unknown"
+     singer: "Favorite"
     },
     {
-     name: "20. Unknown",
-     path: "mp3/20.mp3",
+     name: "20. Keya Keya|Akshay|DJ",
+     path: "mp3/B/20.mp3",
      img: "img/20.jpg",
-     singer: "Unknown"
+     singer: "DJ|Remix"
     },
     {
-     name: "21. Unknown",
-     path: "mp3/21.mp3",
+     name: "21. Kur Kura Ra",
+     path: "mp3/C/21.mp3",
      img: "img/21.jpg",
-     singer: "Unknown"
+     singer: "DJ|Remix"
     },
     {
-     name: "22. Unknown",
-     path: "mp3/22.mp3",
+     name: "22. Lage Ura dhura",
+     path: "mp3/C/22.mp3",
      img: "img/22.jpg",
-     singer: "Unknown"
+     singer: "DJ|Remix"
     },
     {
      name: "23. Unknown",
-     path: "mp3/23.mp3",
+     path: "mp3/C/23.mp3",
      img: "img/23.jpg",
      singer: "Unknown"
     },
     {
-     name: "24. Unknown",
-     path: "mp3/24.mp3",
+     name: "24. Industry|DJ",
+     path: "mp3/C/24.mp3",
      img: "img/24.jpg",
-     singer: "Unknown"
+     singer: "Remix"
     },
     {
      name: "25. Unknown",
-     path: "mp3/25.mp3",
+     path: "mp3/C/25.mp3",
      img: "img/25.jpg",
-     singer: "Unknown"
+     singer: "Remix|DJ"
     },
     {
-     name: "26. Unknown",
-     path: "mp3/26.mp3",
+     name: "26. Middle Of The Night",
+     path: "mp3/C/26.mp3",
      img: "img/26.jpg",
-     singer: "Unknown"
+     singer: "Remix"
     },
     {
-     name: "27. Unknown",
-     path: "mp3/27.mp3",
+     name: "27. Moron o jodi ase",
+     path: "mp3/C/27.mp3",
      img: "img/27.jpg",
-     singer: "Unknown"
+     singer: "Remix|DJ"
     },
     {
-     name: "28. Unknown",
-     path: "mp3/28.mp3",
+     name: "28. Naja Naja",
+     path: "mp3/C/28.mp3",
      img: "img/28.jpg",
-     singer: "Unknown"
+     singer: "Remix"
     },
     {
-     name: "29. Unknown",
-     path: "mp3/29.mp3",
+     name: "29. Pagal banaibe|Bhujpuri",
+     path: "mp3/C/29.mp3",
      img: "img/29.jpg",
-     singer: "Unknown"
-    },
-{
-     name: "30. Unknown",
-     path: "mp3/30.mp3",
-     img: "img/30.jpg",
-     singer: "Unknown"
+     singer: "Remix|DJ"
     },
     {
-     name: "31. Unknown",
-     path: "mp3/31.mp3",
+     name: "30. Palang Sawanke",
+     path: "mp3/C/30.mp3",
+     img: "img/30.jpg",
+     singer: "Bhujpuri"
+    },
+    {
+     name: "31. Mujh se sadi karogi|Remix|DJ",
+     path: "mp3/D/31.mp3",
      img: "img/31.jpg",
-     singer: "Unknown"
+     singer: "Remix|DJ"
     },
     {
      name: "32. Unknown",
-     path: "mp3/32.mp3",
+     path: "mp3/D/32.mp3",
      img: "img/32.jpg",
      singer: "Unknown"
     },
     {
      name: "33. Unknown",
-     path: "mp3/33.mp3",
+     path: "mp3/D/33.mp3",
      img: "img/33.jpg",
-     singer: "Unknown"
+     singer: "Bhujpuri"
     },
     {
-     name: "34. Unknown",
-     path: "mp3/34.mp3",
+     name: "34. Sundle Sundle",
+     path: "mp3/D/34.mp3",
      img: "img/34.jpg",
-     singer: "Unknown"
+     singer: "Bhujpuri|DJ|Remix"
     },
     {
-     name: "35. Unknown",
-     path: "mp3/35.mp3",
+     name: "35. চায়ের কাপে তোমার যত রাগ",
+     path: "mp3/D/35.mp3",
      img: "img/35.jpg",
-     singer: "Unknown"
+     singer: "Bangla|Album"
     },
     {
-     name: "36. Unknown",
-     path: "mp3/36.mp3",
+     name: "36. Top Talker",
+     path: "mp3/D/36.mp3",
      img: "img/36.jpg",
-     singer: "Unknown"
+     singer: "Hindi|Badsha|"
     },
     {
-     name: "37. Unknown",
-     path: "mp3/37.mp3",
+     name: "37. দাদা আমার যেমন তেমন",
+     path: "mp3/D/37.mp3",
      img: "img/37.jpg",
-     singer: "Unknown"
+     singer: "DJ|Remix"
     },
     {
-     name: "38. Unknown",
-     path: "mp3/38.mp3",
+     name: "38. বরিশালের লঞ্চ",
+     path: "mp3/D/38.mp3",
      img: "img/38.jpg",
-     singer: "Unknown"
+     singer: "DJ|নার্গিস"
     },
     {
-     name: "39. Unknown",
-     path: "mp3/39.mp3",
+     name: "39. রাগ করোনা মনের মানুষ",
+     path: "mp3/D/39.mp3",
      img: "img/39.jpg",
-     singer: "Unknown"
-}
+     singer: "DJ|Remix"
+    }
 
 ];
 /*you can add more song & images from you computer*/
